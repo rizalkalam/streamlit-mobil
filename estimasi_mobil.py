f9103087f4f42f74dd730bee187c70ae69ea07a8
Rizal Kalam <rizalkalam78@gmail.com>
2025-12-09 23:16:10 +0700

    first commit

---
+++ estimasi_mobil.py
@@ -0,0 +1,21 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_mobil_ford.sav', 'rb'))
+
+st.title('Estimasi Harga Mobil Bekas Ford')
+
+year = st.number_input('Input Tahun Mobil')
+mileage = st.number_input('Input Jarak Tempuh (dalam mil)')
+tax = st.number_input('Input Pajak Tahunan (dalam £)')
+mpg = st.number_input('Input konsumsi bahan bakar (MPG)')
+engine_size = st.number_input('Input engine size')
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engine_size]]
+    )
+    st.write(f'Estimasi Harga Mobil Bekas Ford: £{predict[0]:.2f}', predict)
+    st.write('Estimasi Harga Mobil Bekas Ford dalam Rupiah: Rp{:,}'.format(int(predict*19000)))
